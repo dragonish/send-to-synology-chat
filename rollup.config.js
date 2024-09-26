b07d73018fbf6a08dd62eeb61963d4d4e673bf47
dragonish <no.web.developer@outlook.com>
2024-09-26 21:58:37 +0800

    fix: fix action not working

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,28 @@
 import typescript from 'rollup-plugin-typescript2';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
 
 export default {
   input: 'src/main.ts',
   output: [
     {
-      file: 'main.js',
+      file: 'main.cjs',
       format: 'cjs',
       sourcemap: false,
     },
   ],
+  context: 'this',
   plugins: [
     typescript({
       tsconfig: './tsconfig.json',
+    }),
+    resolve(),
+    commonjs(),
+    babel({
+      babelHelpers: 'bundled',
+      presets: ['@babel/preset-env'],
+      exclude: 'node_modules/**',
     }),
   ],
 };
